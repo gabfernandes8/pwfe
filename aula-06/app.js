b9744d1731220ef120216c3e0298a0dcdbcf1ff0
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2023-09-06 09:45:53 -0300

    aa

--- aula-06/app.js
+++ aula-06/app.js
@@ -1,1 +1,24 @@
 'use strict'
+
+async function pegarImagens() {
+    const url = 'https://dog.ceo/api/breed/corgi/images'
+    const response = await fetch(url)
+    const imagens = await response.json()
+
+    return imagens.message
+}
+
+function criarTagImg(imagem){
+    const galeria = document.getElementById('galeria')
+    const tagImg = document.createElement('img')
+    tagImg.src =imagem
+
+    galeria.appendChild(tagImg)
+}
+
+async function carregarFotos(){
+    const imagens = await pegarImagens()
+    imagens.forEach(criarTagImg)
+}
+
+carregarFotos()
