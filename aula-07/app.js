676843c1aa7cb4e60295d40d59415412e03e90c2
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2023-09-13 12:07:30 -0300

    aaa

--- aula-07/app.js
+++ aula-07/app.js
@@ -1,40 +1,28 @@
 'use strict'
 
-const input = document.getElementById('input-cep')
-const endereco = document.getElementById('endereco')
-const bairro = document.getElementById('bairro')
-const city = document.getElementById('city')
-const estado = document.getElementById('estado')
+const inputCep = document.getElementById('input')
+
+const completarDados = (pegarCep) => {
+    const endereco = document.getElementById('endereco').textContent = pegarCep.logradouro
+    const bairro = document.getElementById('bairro').textContent = pegarCep.bairro
+    const city = document.getElementById('city').textContent = pegarCep.localidade
+    const estado = document.getElementById('estado').textContent = pegarCep.uf
+}
 
 const pegarCep = async()  =>{
-    let inputCep = document.getElementById('input-cep').value
-    const url = 'viacep.com.br/ws/' + inputCep + '/json/'
+    let inputCep = document.getElementById('input').value
+    const url = 'https://viacep.com.br/ws/' + inputCep + '/json/'
 
     const response = await fetch(url)
-    const cepInfo = await response.json()
-    console.log(cepInfo)
-    return cepInfo
+    const cep = await response.json()
+
+    completarDados(cep)
 }
 
-const completarDados = () => {
+inputCep.addEventListener('blur', pegarCep)
 
-    const infos = pegarCep()
-
-    endereco.textContent
-    bairro.textContent
-    city.textContent
-    estado.textContent
-}
-
-const returnCep = async() =>{
-    const cep = await pegarCep()
-
-}
-
-// input.addEventListener('keyup', function(e){
-//     if (e.key === 'Enter'){
-//         returnCep()
-//     }
-// })
-
-returnCep()
+inputCep.addEventListener('keyup', function(e){
+    if (e.key === 'Enter'){
+        pegarCep()
+    }
+})
