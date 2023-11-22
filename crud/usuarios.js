727a406048bfde867af531c3c8fcecb63953c651
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2023-11-22 11:00:02 -0300

    crud

--- crud/usuarios.js
+++ crud/usuarios.js
@@ -20,8 +20,8 @@ const cadastrarUsuario = async(usuario) => {
     }
 
     const response = await fetch(url, options)
-
-    
+    console.log(response.ok)
+    return response.ok
 }
 
 const deletarUsuario = async(id) => {
@@ -35,9 +35,31 @@ const deletarUsuario = async(id) => {
     return response.ok
 }
 
-const novoUsuario = {
-    "nome": "Ryan Alves",
-    "email": "ryan@gmail.com"
+const atualizarUsuario = async(usuario) => {
+    const url = `http://10.107.140.2:8080/usuarios/${usuario.id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
+
+    const response = await fetch(url, options)
+    console.log(response.ok)
+    return response.ok
 }
 
-deletarUsuario(23)
+const usuarioAtualizado = {
+    "nome": 'Gabriela',
+    "email": 'gab@gmail.com',
+    "id":'22'
+}
+
+const novoUsuario = {
+    "nome": "Gabriela",
+    "email": "gabriela@gmail.com"
+}
+
+cadastrarUsuario(novoUsuario)
+// atualizarUsuario(usuarioAtualizado)
